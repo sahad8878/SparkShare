b5235ab28e6c9394075df9541b7f4bfcf80b9e39
sahad <msahadpp@gmail.com>
2023-04-14 11:12:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+
+
+  return (
+    <div className=" h-screen w-full bg-[#f5e9e2] "  >
+
+         <div className="h-20 w-screen bg-[#e3b5a4]">
+          <p className="flex justify-center content-center p-7 text-gray-50 ">SparkShare</p>
+         </div>
+         <div>
+          <form action=""></form>
+         </div>
+    </div>
+  )
+}
+
+export default App

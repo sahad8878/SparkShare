239af7071ece556b16837bf8df6296165015e141
sahad <msahadpp@gmail.com>
2023-04-14 11:38:33 +0530

    form component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Form from "./Components/Form"
+import Table from "./Components/Table"
 
 
 function App() {
@@ -7,10 +9,13 @@ function App() {
     <div className=" h-screen w-full bg-[#f5e9e2] "  >
 
          <div className="h-20 w-screen bg-[#e3b5a4]">
-          <p className="flex justify-center content-center p-7 text-gray-50 ">SparkShare</p>
+          <p className="flex justify-center content-center p-7 text-gray-50 text-lg font-sans font-bold ">SparkShare</p>
          </div>
          <div>
-          <form action=""></form>
+         <Form/>
+         </div>
+         <div>
+          <Table/>
          </div>
     </div>
   )
---
+++ src/Components/Form.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Form() {
+  return (
+
+
+    <div className='flex justify-center text-center content-center mt-20'> 
+    <div className='flex flex-col '>
+      <label htmlFor=""> Name</label>
+      <input type="text" className='w-60' />
+      <label htmlFor="">ideas</label>
+      <textarea/>
+    </div>
+
+    </div>
+  )
+}
+
+export default Form
